fd945fba1a589e4e6b466db98a578b4fe1cf76ec
G4IXT <ian.jefferson@btinternet.com>
2025-11-19 22:15:25 +0000

    Update QtTinySA.py

--- src/QtTinySA.py
+++ src/QtTinySA.py
@@ -7,7 +7,7 @@
 # Compilation mode, support OS-specific options
 # nuitka-project-if: {OS} in ("Windows", "Linux", "Darwin", "FreeBSD"):
 #    nuitka-project: --mode=standalone
-#    nuitka-project: --enable-plugin=PySide6
+#    nuitka-project: --enable-plugin=pyside6
 #    nuitka-project: --include-qt-plugins=sqldrivers
 #    nuitka-project: --include-data-file=QtTSAprefs.db=./
 #    nuitka-project: --remove-output
