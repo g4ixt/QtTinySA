3db98447556c7a592c264e2af1d20d03a3d022ad
Ian <76836635+g4ixt@users.noreply.github.com>
2026-07-27 19:47:29 +0100

    Update QtTinySA.py

--- src/QtTinySA.py
+++ src/QtTinySA.py
@@ -8,7 +8,7 @@
 # nuitka-project: --include-qt-plugins=sqldrivers,designer
 # nuitka-project: --include-data-file=QtTSAprefs.db=./
 # nuitka-project: --include-data-files=./modules/*baseline.txt=modules/
-# nuitka-project: --include-data-files=*.ui=/
+# nuitka-project: --include-data-files=*.ui=./
 # nuitka-project: --nofollow-import-to=tkinter,pandas,setuptools,tk,wheel,zipp,pyyaml
 # nuitka-project: --nofollow-import-to=packaging,altgraph,mkl,fortran,matlab
 # nuitka-project: --remove-output
